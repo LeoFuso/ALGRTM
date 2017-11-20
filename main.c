0ff499e4549bbe456b1f42367b2aff0abcfd7050
LeoFuso <leonardofusonuzzo@gmail.com>
2017-11-20 15:45:50 -0200

    desisto

--- main.c
+++ main.c
@@ -5,7 +5,9 @@
 struct Node {
     char name[2];
     int last_parent;
+    int last_children;
     struct Node *parents;
+    struct Node *children;
 };
 
 typedef struct {
@@ -24,6 +26,7 @@ void kruskal(Path *original_path, int original_path_size, Path *kruskal_path, in
              struct Node *original_nodes, struct Node *kruskal_nodes, int nodes_qtd);
 
 int check_parent(struct Node node_1, struct Node node_2, struct Node *kruskal_nodes);
+int check_children(struct Node node_1, struct Node node_2, struct Node *kruskal_nodes);
 
 int main(void) {
 
@@ -114,10 +117,12 @@ void kruskal(Path *original_path, int original_path_size, Path *kruskal_path, in
         struct Node s_from_node = kruskal_nodes[from_node];
         struct Node s_to_node = kruskal_nodes[to_node];
 
-        if (check_parent(s_from_node,s_to_node, kruskal_nodes) == 0) {
+        if (check_parent(s_from_node,s_to_node, kruskal_nodes) == 0 &&
+                check_children(s_from_node,s_to_node, kruskal_nodes) == 0) {
 
                 kruskal_path[kruskal_path_control++] = i_path;
                 kruskal_nodes[from_node].parents[kruskal_nodes[from_node].last_parent++] = kruskal_nodes[to_node];
+                kruskal_nodes[to_node].children[kruskal_nodes[to_node].last_children++] = kruskal_nodes[from_node];
 
         }
 
@@ -139,7 +144,7 @@ int check_parent(struct Node node_1, struct Node node_2, struct Node *kruskal_no
             if(strcmp(node_1.name, node_2.parents[j].name) == 0){
                 return 1;
             }else{
-                return check_parent(node_1, node_2.parents[j], kruskal_nodes);
+                check_parent(node_1, node_2.parents[j], kruskal_nodes);
             }
         }
     }
@@ -149,7 +154,7 @@ int check_parent(struct Node node_1, struct Node node_2, struct Node *kruskal_no
             if(strcmp(node_1.parents[j].name, node_2.name) == 0){
                 return 1;
             }else{
-                return check_parent(node_2, node_1.parents[j], kruskal_nodes);
+                check_parent(node_2, node_1.parents[j], kruskal_nodes);
             }
         }
     }
@@ -159,7 +164,46 @@ int check_parent(struct Node node_1, struct Node node_2, struct Node *kruskal_no
             if(strcmp(node_1.parents[i].name, node_2.parents[j].name) == 0){
                 return 1;
             }else{
-                return check_parent(node_1.parents[i], node_2.parents[j], kruskal_nodes);
+                check_parent(node_1.parents[i], node_2.parents[j], kruskal_nodes);
+            }
+        }
+    }
+    return 0;
+}
+
+int check_children(struct Node node_1, struct Node node_2, struct Node *kruskal_nodes) {
+    int i;
+    int j;
+
+    if(node_1.last_children == 0 && node_2.last_children == 0)
+        return 0;
+
+    if(node_1.last_children == 0){
+        for (j = 0; j < node_2.last_children ; ++j) {
+            if(strcmp(node_1.name, node_2.children[j].name) == 0){
+                return 1;
+            }else{
+                check_parent(node_1, node_2.children[j], kruskal_nodes);
+            }
+        }
+    }
+
+    if(node_2.last_children == 0){
+        for (j = 0; j < node_1.last_children ; ++j) {
+            if(strcmp(node_1.children[j].name, node_2.name) == 0){
+                return 1;
+            }else{
+                check_parent(node_2, node_1.children[j], kruskal_nodes);
+            }
+        }
+    }
+
+    for (i = 0; i < node_1.last_children ; ++i) {
+        for (j = 0; j < node_2.last_children ; ++j) {
+            if(strcmp(node_1.children[i].name, node_2.children[j].name) == 0){
+                return 1;
+            }else{
+                check_parent(node_1.children[i], node_2.children[j], kruskal_nodes);
             }
         }
     }
@@ -170,33 +214,44 @@ void mount_paths_nodes(Path *p, struct Node *n, int nodes_qtd) {
 
     strcpy(n[0].name, "A");
     n[0].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+    n[0].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
 
     strcpy(n[1].name, "B");
     n[1].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+    n[1].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
+
 
     strcpy(n[2].name, "C");
     n[2].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+    n[2].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
+
 
     strcpy(n[3].name, "D");
     n[3].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+    n[3].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
+
 
     //B --> A = 25
     p[0].distance = 25;
     p[0].node_a = n[1];
     p[0].node_b = n[0];
     n[1].parents[n[1].last_parent++] = n[0];
+    n[0].children[n[0].last_children++] = n[1];
 
     //C --> B = 50
     p[1].distance = 50;
     p[1].node_a = n[2];
     p[1].node_b = n[1];
     n[2].parents[n[2].last_parent++] = n[1];
+    n[1].children[n[1].last_children++] = n[2];
 
     //D --> C = 10
     p[2].distance = 10;
     p[2].node_a = n[3];
     p[2].node_b = n[2];
     n[3].parents[n[3].last_parent++] = n[2];
+    n[2].children[n[2].last_children++] = n[3];
+
 
 
     //D --> A = 25
@@ -204,12 +259,15 @@ void mount_paths_nodes(Path *p, struct Node *n, int nodes_qtd) {
     p[3].node_a = n[3];
     p[3].node_b = n[0];
     n[3].parents[n[3].last_parent++] = n[0];
+    n[0].children[n[0].last_children++] = n[3];
+
 
     //C --> A = 40
     p[4].distance = 40;
     p[4].node_a = n[2];
     p[4].node_b = n[0];
     n[2].parents[n[2].last_parent++] = n[0];
+    n[0].children[n[0].last_children++] = n[2];
 }
 
 void print_nodes(struct Node *n, int nodes_qtd) {
@@ -254,6 +312,7 @@ void setup(struct Node *original_nodes, struct Node *kruskal_nodes, int nodes_qt
     for (k = 0; k < nodes_qtd; ++k) {
         strcpy(kruskal_nodes[k].name, original_nodes[k].name);
         kruskal_nodes[k].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+        kruskal_nodes[k].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
     }
 
 }
