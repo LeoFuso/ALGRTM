6ed3a31d4630011f947929f01d4ea547083bc564
LeoFuso <leonardofusonuzzo@gmail.com>
2017-11-20 16:13:20 -0200

    Geez

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Leonardo Fuso Nuzzo         31500129
+//Gustavo Boldrini Custoias   31553826
+
 struct Node {
     char name[2];
     int last_parent;
@@ -30,8 +33,8 @@ int check_children(struct Node node_1, struct Node node_2, struct Node *kruskal_
 
 int main(void) {
 
-    int nodes_qtd = 4;
-    int original_path_size = 5;
+    int nodes_qtd = 5;
+    int original_path_size = 10;
     int kruskal_path_size = nodes_qtd - 1;
 
     Path *original_path = malloc(original_path_size * sizeof(Path));
@@ -41,7 +44,7 @@ int main(void) {
     struct Node *kruskal_nodes = malloc(nodes_qtd * sizeof(struct Node));
 
 
-    mount_paths_nodes(original_path, original_nodes, nodes_qtd);
+    mount_paths_nodes_2(original_path, original_nodes, nodes_qtd);
 
 
     printf("\n\nORIGINAL NODES:\n");
@@ -87,9 +90,6 @@ void kruskal(Path *original_path, int original_path_size, Path *kruskal_path, in
     int kruskal_path_control = 0;
 
     for (i = 0; i < original_path_size; ++i) {
-
-        printf("\n\nITERATION %d:", i+1);
-        print_nodes(kruskal_nodes, nodes_qtd);
 
         i_path = original_path[i];
 
@@ -268,6 +268,116 @@ void mount_paths_nodes(Path *p, struct Node *n, int nodes_qtd) {
     p[4].node_b = n[0];
     n[2].parents[n[2].last_parent++] = n[0];
     n[0].children[n[0].last_children++] = n[2];
+}
+
+void mount_paths_nodes_2(Path *p, struct Node *n, int nodes_qtd) {
+
+    strcpy(n[0].name, "V1");
+    n[0].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+    n[0].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
+
+    strcpy(n[1].name, "V2");
+    n[1].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+    n[1].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
+
+
+    strcpy(n[2].name, "V3");
+    n[2].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+    n[2].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
+
+
+    strcpy(n[3].name, "V4");
+    n[3].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+    n[3].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
+
+    strcpy(n[4].name, "V5");
+    n[4].parents = malloc((nodes_qtd - 1) * sizeof(struct Node));
+    n[4].children = malloc((nodes_qtd - 1) * sizeof(struct Node));
+
+
+    //V2 --> V1 = 9
+    p[0].distance = 9;
+    p[0].node_a = n[1];
+    p[0].node_b = n[0];
+
+    n[1].parents[n[1].last_parent++] = n[0];
+    n[0].children[n[0].last_children++] = n[1];
+
+    //V3 --> V2 = 8
+    p[1].distance = 8;
+    p[1].node_a = n[2];
+    p[1].node_b = n[1];
+
+    n[2].parents[n[2].last_parent++] = n[1];
+    n[1].children[n[1].last_children++] = n[2];
+
+    //V4 --> V3 = 10
+    p[2].distance = 10;
+    p[2].node_a = n[3];
+    p[2].node_b = n[2];
+
+    n[3].parents[n[3].last_parent++] = n[2];
+    n[2].children[n[2].last_children++] = n[3];
+
+
+    //V5 --> V4 = 7
+    p[3].distance = 7;
+    p[3].node_a = n[4];
+    p[3].node_b = n[3];
+
+    n[4].parents[n[4].last_parent++] = n[3];
+    n[3].children[n[3].last_children++] = n[4];
+
+
+    //V3 --> V1 = 6
+    p[4].distance = 6;
+    p[4].node_a = n[2];
+    p[4].node_b = n[0];
+
+    n[2].parents[n[2].last_parent++] = n[0];
+    n[0].children[n[0].last_children++] = n[2];
+
+    //V4 --> V1 = 15
+    p[5].distance = 15;
+    p[5].node_a = n[3];
+    p[5].node_b = n[0];
+
+    n[3].parents[n[3].last_parent++] = n[0];
+    n[0].children[n[0].last_children++] = n[3];
+
+    //V5 --> V1 = 14
+    p[6].distance = 14;
+    p[6].node_a = n[4];
+    p[6].node_b = n[0];
+
+    n[4].parents[n[4].last_parent++] = n[0];
+    n[0].children[n[0].last_children++] = n[4];
+
+    //V4 --> V2 = 17
+    p[7].distance = 17;
+    p[7].node_a = n[3];
+    p[7].node_b = n[1];
+
+    n[3].parents[n[3].last_parent++] = n[1];
+    n[1].children[n[1].last_children++] = n[3];
+
+    //V5 --> V2 = 12
+    p[8].distance = 12;
+    p[8].node_a = n[4];
+    p[8].node_b = n[1];
+
+    n[4].parents[n[4].last_parent++] = n[1];
+    n[1].children[n[1].last_children++] = n[4];
+
+    //V5 --> V3 = 9
+    p[9].distance = 9;
+    p[9].node_a = n[4];
+    p[9].node_b = n[2];
+
+    n[4].parents[n[4].last_parent++] = n[2];
+    n[2].children[n[2].last_children++] = n[4];
+
+
 }
 
 void print_nodes(struct Node *n, int nodes_qtd) {
